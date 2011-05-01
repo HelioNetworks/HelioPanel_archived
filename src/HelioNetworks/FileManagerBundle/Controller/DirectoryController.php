0f7dceeebf17be2a34f48dd65814ee0330bac05c
Geoff <webmaster@geoffish.heliohost.org>
2011-05-01 12:39:40 -0500

    Added file manager bundle.

---
+++ src/HelioNetworks/FileManagerBundle/Controller/DirectoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace HelioNetworks\FileManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DirectoryController extends Controller {
+
+    function create() {}
+
+    function delete() {}
+
+    function rename() {}
+
+    /**
+     * For those of you who don't know what
+     * enumerate means, it's basically a
+     * synonym for 'list'. The problem
+     * is that 'list' is a PHP built-in
+     * function that is already taken
+     */
+    function enumerate() {}
+}
---
+++ src/HelioNetworks/FileManagerBundle/Controller/FileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HelioNetworks\FileManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class FileController extends Controller {
+
+    public function create() {}
+
+    public function edit() {}
+
+    public function rename() {}
+
+    public function delete() {}
+}
