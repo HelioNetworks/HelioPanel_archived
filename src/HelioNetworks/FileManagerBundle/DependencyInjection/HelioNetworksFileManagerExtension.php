3ca7ec31643349a9fc65d97fa81b2084348dd7ea
Geoff <webmaster@geoffish.heliohost.org>
2011-05-03 21:24:20 -0500

    Added Dependancy Injection.

---
+++ src/HelioNetworks/FileManagerBundle/DependencyInjection/HelioNetworksFileManagerExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HelioNetworks\FileManagerBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class HelioNetworksFileManagerExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
