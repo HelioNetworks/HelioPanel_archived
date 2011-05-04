9b3374f42a5d066e619ee627e88106e46f8b9952
Geoff <webmaster@geoffish.heliohost.org>
2011-05-03 20:29:12 -0500

    Added rename file function.

--- src/HelioNetworks/FileManagerBundle/Controller/FileController.php
+++ src/HelioNetworks/FileManagerBundle/Controller/FileController.php
@@ -2,7 +2,9 @@
 
 namespace HelioNetworks\FileManagerBundle\Controller;
 
-use HelioNetworks\FileManagerBundle\Form\RenameType;
+use HelioNetworks\FileManagerBundle\Form\RenameFileFormHandler;
+
+use HelioNetworks\FileManagerBundle\Form\RenameFileFormType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,11 +14,24 @@ class FileController extends Controller {
 
     public function edit() {}
 
-    public function rename() {
+    public function rename()
+    {
 
-        $form = $this->get('form.factory')->create(new RenameType());
+        $form = $this->get('form.factory')->create(new RenameFileFormType());
 
+        $request = $this->get('request');
 
+        if($request->getMethod() == 'POST') {
+
+            $form->bindRequest($request);
+
+            if($form->isValid()) {
+
+                $handler = new RenameFileFormHandler($form, $request);
+
+                $handler->process();
+            }
+        }
     }
 
     public function delete() {}
