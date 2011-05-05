24a759285e8314e8556ad294fa547598d6d56eb2
Geoff <webmaster@geoffish.heliohost.org>
2011-05-04 21:34:04 -0500

    Updated DependencyInjection.

--- src/HelioNetworks/FileManagerBundle/DependencyInjection/HelioNetworksFileManagerExtension.php
+++ src/HelioNetworks/FileManagerBundle/DependencyInjection/HelioNetworksFileManagerExtension.php
@@ -10,7 +10,10 @@ class HelioNetworksFileManagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $container->setParameter('helionetworks_filemanager.base_directory', $baseDirectory);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+
     }
 }
