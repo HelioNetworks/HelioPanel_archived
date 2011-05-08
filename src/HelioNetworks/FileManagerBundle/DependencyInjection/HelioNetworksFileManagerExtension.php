cce9479d94561bddc5ca0f1e1182fb90dd929ce0
Geoff <webmaster@geoffish.heliohost.org>
2011-05-08 12:45:23 -0500

    Updated file abstraction DIC.

--- src/HelioNetworks/FileManagerBundle/DependencyInjection/HelioNetworksFileManagerExtension.php
+++ src/HelioNetworks/FileManagerBundle/DependencyInjection/HelioNetworksFileManagerExtension.php
@@ -10,10 +10,31 @@ class HelioNetworksFileManagerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $container->setParameter('helionetworks_filemanager.filesystem.base_directory', '/home1/area52/public_html/test_dir/');
-
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
+    }
+
+    public function configureFilesystemAdapter(array $configs, ContainerBuilder $container)
+    {
+        // add the default configuration for the local filesystem
+        if ($container->hasDefinition('helionetworks_filemanager.adapter.filesystem.local') && isset($config['filesystem']['helionetworks_filemanager.adapter.filesystem.local'])) {
+            $definition = $container->getDefinition('helionetworks_filemanager.adapter.filesystem.local');
+            $configuration =  $config['filesystem']['helionetworks_filemanager.adapter.filesystem.local'];
+            $definition->addArgument($configuration['directory']);
+            $definition->addArgument($configuration['create']);
+        }
+
+        // add the default configuration for the FTP filesystem
+        if ($container->hasDefinition('helionetworks_filemanager.adapter.filesystem.ftp') && isset($config['filesystem']['helionetworks_filemanager.adapter.filesystem.ftp'])) {
+            $definition = $container->getDefinition('helionetworks_filemanager.adapter.filesystem.ftp');
+            $configuration =  $config['filesystem']['helionetworks_filemanager.adapter.filesystem.ftp'];
+            $definition->addArgument($configuration['directory']);
+            $definition->addArgument($configuration['username']);
+            $definition->addArgument($configuration['password']);
+            $definition->addArgument($configuration['port']);
+            $definition->addArgument($configuration['passive']);
+            $definition->addArgument($configuration['create']);
+        }
     }
 }
