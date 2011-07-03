56cdb7fdbd961c3835f583b86598c504a7d4a33f
xaav <xaav@xaav.tk>
2011-07-03 14:54:59 -0500

    Lazy loading connection.

--- src/HelioNetworks/HelioHostAccountBundle/Manager/HelioHostAccountManager.php
+++ src/HelioNetworks/HelioHostAccountBundle/Manager/HelioHostAccountManager.php
@@ -8,19 +8,40 @@ class HelioHostAccountManager
 {
     protected $connection;
 
+    protected $server;
+    protected $username;
+    protected $password;
+    protected $database;
+
+    protected function initialize()
+    {
+        if(!$this->connection)
+        {
+            $this->connection = mysql_connect($this->server, $this->username, $this->password);
+            mysql_select_db($this->database, $this->connection);
+        }
+    }
+
     public function __construct($server, $username, $password, $database)
     {
-        $this->connection = mysql_connect($server, $username, $password);
-        mysql_select_db($database, $this->connection);
+        $this->server = $server;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
     }
 
     public function getAccountRepository()
     {
+        $this->initialize();
         return new AccountRepository($this->connection);
     }
 
     public function __destruct()
     {
-        mysql_close($this->connection);
+        if($this->connection)
+        {
+            mysql_close($this->connection);
+            $this->connection = null;
+        }
     }
 }
