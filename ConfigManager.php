8496fb6225184b5d8107af693b3caaa46b8d26a2
xaav <xaav@xaav.tk>
2011-08-10 20:12:29 -0500

    Updated configmanager.

--- ConfigManager.php
+++ ConfigManager.php
@@ -4,12 +4,14 @@ class ConfigManager
 {
     public function setConfig(array $config)
     {
-        $configFile = '<?php return '.var_dump($config);
+        $configFile = '<?php $config = '.var_dump($config);
         file_put_contents(__DIR__.'/config.php', $configFile);
     }
 
     public function getConfig()
     {
         require __DIR__.'/config.php';
+
+        return $config;
     }
 }
