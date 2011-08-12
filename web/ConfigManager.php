e019e020f0089f60955b630f2321a1ae1bcb6bdf
xaav <xaav@xaav.tk>
2011-08-12 14:14:58 -0500

    Added ability to change config location.

--- web/ConfigManager.php
+++ web/ConfigManager.php
@@ -2,15 +2,29 @@
 
 class ConfigManager
 {
+    /**
+     * Location to the config file.
+     */
+    protected $configLocation;
+
+    public function __construct($config_location = null)
+    {
+        if(!$config_location) {
+            $config_location = __DIR__.'/config.php';
+        }
+
+        $this->configLocation = $config_location;
+    }
+
     public function setConfig(array $config)
     {
         $configFile = '<?php $config = '.var_export($config, true).';';
-        file_put_contents(__DIR__.'/config.php', $configFile);
+        file_put_contents($this->configLocation, $configFile);
     }
 
     public function getConfig()
     {
-        require __DIR__.'/config.php';
+        require $this->configLocation;
 
         return $config;
     }
