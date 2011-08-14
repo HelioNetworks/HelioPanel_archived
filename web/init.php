87462f8bffba45a818b30a0356594bb17f47fbc7
xaav <xaav@xaav.tk>
2011-08-14 16:11:57 -0500

    Separate init logic.

---
+++ web/init.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+// Make sure the user is logged in
+if (!isset($_SESSION['username'])) {
+	header("location:login.php");
+	die();
+}
+
+function installHook($username, $password, $redirect = 'http://central.heliopanel.heliohost.org')
+{
+    $url = 'http://heliopanel.heliohost.org/install/autoinstall.php?';
+    $query = http_build_query(array(
+        'username' => $username,
+        'password' => $password,
+        'redirect' => $redirect,
+    ));
+
+    header('location:'.$url.$query);
+    die();
+}
+
+// Include the configuration
+require 'FileRepository.php';
+require 'ConfigManager.php';
+
+$username = $_SESSION['username'];
+$configManager = new ConfigManager();
+$config = $configManager->getConfig();
+$currentConfig = $config[$username];
+$fileRepository = new FileRepository($currentConfig['hook_php'], $currentConfig['hook_auth']);
+
+// Get the user's home directory
+if (file_exists('/home/'.$username)) {
+    $homedir = '/home/'.$username;
+} elseif (file_exists('/home1/'.$username)) {
+    $homedir = '/home1/'.$username;
+} else {
+	die ('Fatal Error: Cannot find home directory!');
+}
