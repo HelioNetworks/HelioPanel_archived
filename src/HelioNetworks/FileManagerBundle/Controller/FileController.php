f93bcab7ddca8cd9c4f3eb91f88137c1d4087b66
xaav <xaav@xaav.tk>
2011-08-17 18:12:27 -0500

    Add file and directory controllers.

---
+++ src/HelioNetworks/FileManagerBundle/Controller/FileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HelioNetworks\FileManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class FileController extends Controller
+{
+}
