3ac5754027219b75ae06ca7118539a95e3f8ffad
xaav <xaav@xaav.tk>
2011-08-21 15:10:06 -0500

    Fix use reference.

--- src/HelioNetworks/HelioPanelBundle/Controller/AccountController.php
+++ src/HelioNetworks/HelioPanelBundle/Controller/AccountController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
+use HelioNetworks\HelioPanelBundle\Entity\Account;
 
 class AccountController extends Controller
 {
