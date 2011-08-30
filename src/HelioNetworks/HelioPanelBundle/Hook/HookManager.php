dbc0f8e083472a93da3425b6b7972cfe6967d7dc
xaav <xaav@xaav.tk>
2011-08-29 19:48:33 -0500

    Complete HookManager class.

--- src/HelioNetworks/HelioPanelBundle/Hook/HookManager.php
+++ src/HelioNetworks/HelioPanelBundle/Hook/HookManager.php
@@ -4,7 +4,50 @@ namespace HelioNetworks\HelioPanelBundle\Hook;
 
 class HookManager
 {
-	public function getSource() {}
+	protected $sections;
 
-	public function addSection(HookSectionInterface $section) {}
+	protected function getStart()
+	{
+		return <<<'PHP'
+<?php
+
+error_reporting(0);
+
+if($_POST['__auth'] !== '%auth%') {
+    echo '403 Unauthorized';
+    die();
+}
+
+PHP;
+	}
+
+	protected function getEnd()
+	{
+		return <<<'PHP'
+
+ob_start();
+$result = call_user_func_array($_POST['__name'], $_POST);
+ob_get_clean();
+
+echo serialize($result);
+PHP;
+	}
+
+	public function getSource()
+	{
+		$source = $this->getStart();
+		foreach ($this->sections as $section) {
+			$source .= sprintf('function %s {', $section->getName());
+			$source .= $section->getCode();
+			$source .= '}';
+		}
+		$source .= $this->getEnd();
+
+		return $source;
+	}
+
+	public function addSection(HookSectionInterface $section)
+	{
+		$this->sections[] = $section;
+	}
 }
