b2d426af053644113490469b563d31a870ff246c
xaav <xaav@xaav.tk>
2011-09-05 14:41:11 -0500

    Add basic routing.

--- src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
+++ src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
@@ -2,18 +2,47 @@
 
 namespace HelioNetworks\SQLBuddyBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
+	protected function getVendorDir()
+	{
+		return dirname($this->get('kernel')->getRootDir()).'/vendor';
+	}
+
+	protected function getSQLBuddyDir()
+	{
+		return $this->getVendorDir().'/sqlbuddy';
+	}
+
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/sqlbuddy/{file}")
      */
-    public function indexAction($name)
+    public function indexAction($file)
     {
-        return array('name' => $name);
+    	ob_start();
+    	require $this->getSQLBuddyDir().'/'.$file;
+
+        return new Response(ob_get_clean());
+    }
+
+    /**
+     * @Route("/sqlbuddy/js/{file}")
+     */
+    public function jsAction($file)
+    {
+    	return new Response(file_get_contents($this->getSQLBuddyDir().'/js/'.$file));
+    }
+
+   	/**
+     * @Route("/sqlbuddy/css/{file}")
+     */
+    public function cssAction($file)
+    {
+    	return new Response(file_get_contents($this->getSQLBuddyDir().'/css/'.$file));
     }
 }
