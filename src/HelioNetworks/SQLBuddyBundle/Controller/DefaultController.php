a8ae9fab98f5131b2483ed71483454961503c862
xaav <xaav@xaav.tk>
2011-09-05 18:18:43 -0500

    Updated default controller.

--- src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
+++ src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
@@ -2,17 +2,26 @@
 
 namespace HelioNetworks\SQLBuddyBundle\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use HelioNetworks\HelioPanelBundle\Controller\HelioPanelAbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class DefaultController extends HelioPanelAbstractController
 {
     /**
-     * @Route("/sqlbuddy/")
+     * @Route("/sqlbuddy/", name="sqlbuddy_index")
      */
     public function indexAction()
     {
+    	$account = $this->getActiveAccount();
+
+    	$_SESSION['sqlbuddy_username'] = $account->getUsername();
+    	$_SESSION['sqlbuddy_password'] = $account->getPassword();
+    	$_SESSION['sqlbuddy_host'] = parse_url($this->getHook()->getUrl(), PHP_URL_HOST);
+
+    	return new RedirectResponse('/sqlbuddy/index.php');
     }
 }
