b344f628006f41187bf97fd4c9959aedac8695bd
xaav <xaav@xaav.tk>
2011-09-05 18:13:16 -0500

    Adjust SQLBuddy location.

--- src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
+++ src/HelioNetworks/SQLBuddyBundle/Controller/DefaultController.php
@@ -9,24 +9,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-	protected function getVendorDir()
-	{
-		return dirname($this->get('kernel')->getRootDir()).'/vendor';
-	}
-
-	protected function getSQLBuddyDir()
-	{
-		return $this->getVendorDir().'/sqlbuddy';
-	}
-
     /**
-     * @Route("/sqlbuddy/(.*)")
+     * @Route("/sqlbuddy/")
      */
     public function indexAction()
     {
-    	ob_start();
-    	require $this->getSQLBuddyDir().'/'.$file;
-
-        return new Response(ob_get_clean());
     }
 }
