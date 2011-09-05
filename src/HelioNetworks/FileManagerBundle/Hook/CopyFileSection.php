3c4fb496a100c4cfda9722ac419c78d0427bce96
xaav <xaav@xaav.tk>
2011-09-04 19:40:49 -0500

    Fix function conflicts.

--- src/HelioNetworks/FileManagerBundle/Hook/CopyFileSection.php
+++ src/HelioNetworks/FileManagerBundle/Hook/CopyFileSection.php
@@ -4,17 +4,17 @@ namespace HelioNetworks\FileManagerBundle\Hook;
 
 use HelioNetworks\HelioPanelBundle\Hook\HookSectionInterface;
 
-class RenameSection implements HookSectionInterface
+class CopyFileSection implements HookSectionInterface
 {
 	public function getName()
 	{
-		return 'rename($source, $dest)';
+		return 'copyFile($source, $dest)';
 	}
 
 	public function getCode()
 	{
 		return <<<'PHP'
-return rename(dirname(__DIR__).'/'.$source, dirname(__DIR__).'/'.$dest);
+return copy(dirname(__DIR__).'/'.$source, dirname(__DIR__).'/'.$dest);
 PHP;
 	}
 }
--- src/HelioNetworks/FileManagerBundle/Hook/RenameFileSection.php
+++ src/HelioNetworks/FileManagerBundle/Hook/RenameFileSection.php
@@ -4,17 +4,17 @@ namespace HelioNetworks\FileManagerBundle\Hook;
 
 use HelioNetworks\HelioPanelBundle\Hook\HookSectionInterface;
 
-class CopySection implements HookSectionInterface
+class RenameFileSection implements HookSectionInterface
 {
 	public function getName()
 	{
-		return 'copy($source, $dest)';
+		return 'renameFile($source, $dest)';
 	}
 
 	public function getCode()
 	{
 		return <<<'PHP'
-return copy(dirname(__DIR__).'/'.$source, dirname(__DIR__).'/'.$dest);
+return rename(dirname(__DIR__).'/'.$source, dirname(__DIR__).'/'.$dest);
 PHP;
 	}
 }
