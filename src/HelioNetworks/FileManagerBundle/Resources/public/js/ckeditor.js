3c26f05d8e2d398384031fc865339eb63b6e0e6d
xaav <xaav@xaav.tk>
2011-09-11 21:49:27 -0500

    Edited src/HelioNetworks/FileManagerBundle/Resources/public/js/ckeditor.js via GitHub

--- src/HelioNetworks/FileManagerBundle/Resources/public/js/ckeditor.js
+++ src/HelioNetworks/FileManagerBundle/Resources/public/js/ckeditor.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-	var editor = $('#editor').ckeditor();
-
-    editor.val($('#file_data').val());
-
-    $('#file_edit').submit(function () {
-		$('#file_data').val(editor.val());
-
-		return true;
+	$('#editor').ckeditor(function () {
+	
+		$('#editor').val($('#file_data').val());
+		
+		$('#file_edit').submit(function () {
+			$('#file_data').val($('#editor').val());
+	
+			return true;
+		});
 	});
 });
