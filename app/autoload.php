e1c878e4f808668cc29a2c90eeb6c87c0bcf64d6
xaav <xaav@xaav.tk>
2011-12-28 13:00:17 -0600

    Remove deps files, update composer.json, move build_bootstrap.

--- app/autoload.php
+++ app/autoload.php
@@ -45,3 +45,5 @@ AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/M
 require_once __DIR__.'/../vendor/swiftmailer/swiftmailer/lib/classes/Swift.php';
 Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/swiftmailer/lib/swift_init.php');
 
+//Require the composer autoloader
+require_once __DIR__.'/../vendor/.composer/autoload.php';
