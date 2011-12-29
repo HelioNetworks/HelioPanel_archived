65aea8afb86cf4ef91e5535b51519f2b0efd362b
xaav <xaav@xaav.tk>
2011-12-29 19:16:39 +0000

    Added test

---
+++ src/HelioNetworks/HelioPanelBundle/Tests/SanityTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HelioNetworks\HelioPanelBundle\Tests;
+
+class SanityTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSanity()
+    {
+        $this->assertTrue(true);
+    }
+}
