97d31da2c9eadd66531cf08ba12fcf346602822e
xaav <xaav@xaav.tk>
2011-12-29 19:32:41 +0000

    Added test case

---
+++ src/HelioNetworks/HelioPanelBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HelioNetworks\HelioPanelBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testLoginScreen()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($crawler->filter('html:contains("Login to HelioPanel")')->count() > 0);
+    }
+}
