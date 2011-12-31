148a4afd305337bb3e733b65805117a5a5c7c3de
xaav <xaav@xaav.tk>
2011-12-31 04:58:59 +0000

    Updated test.

--- src/HelioNetworks/HelioPanelBundle/Tests/Controller/DefaultControllerTest.php
+++ src/HelioNetworks/HelioPanelBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,7 +9,7 @@ class DefaultControllerTest extends WebTestCase
     public function testLoginScreen()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
         $this->assertTrue($crawler->filter('html:contains("Login to HelioPanel")')->count() > 0);
     }
 }
